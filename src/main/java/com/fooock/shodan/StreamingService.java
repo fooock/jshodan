ec3be468f93cb2b7424fa2d2a75a1644e2077bab
newhouse <nhitbh@gmail.com>
2017-02-12 19:23:36 +0100

    Reformatted all project code

--- src/main/java/com/fooock/shodan/StreamingService.java
+++ src/main/java/com/fooock/shodan/StreamingService.java
@@ -1,3 +1,27 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Newhouse (nhitbh at gmail dot com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.fooock.shodan;
 
 import com.fooock.shodan.model.banner.BannerReport;
@@ -7,7 +31,8 @@ import retrofit2.http.Query;
 import rx.Observable;
 
 /**
- * The Streaming API is an HTTP-based service that returns a real-time stream of data collected by Shodan.
+ * The Streaming API is an HTTP-based service that returns a real-time stream of data collected by
+ * Shodan.
  */
 public interface StreamingService {
 
@@ -26,12 +51,13 @@ public interface StreamingService {
      * @param apiKey account api key
      */
     @GET("shodan/alert/{id}")
-    Observable<BannerReport> alert(@Path(Constants.ID) String id, @Query(Constants.KEY) String apiKey);
+    Observable<BannerReport> alert(@Path(Constants.ID) String id,
+                                   @Query(Constants.KEY) String apiKey);
 
     /**
-     * This stream provides ALL of the data that Shodan collects. Use this stream if you need access to everything
-     * and/ or want to store your own Shodan database locally. If you only care about specific ports, please use
-     * the Ports stream.
+     * This stream provides ALL of the data that Shodan collects. Use this stream if you need access
+     * to everything and/ or want to store your own Shodan database locally. If you only care about
+     * specific ports, please use the Ports stream.
      *
      * @param apiKey account api key
      */
@@ -39,20 +65,22 @@ public interface StreamingService {
     Observable<BannerReport> banners(@Query(Constants.KEY) String apiKey);
 
     /**
-     * This stream provides a filtered, bandwidth-saving view of the Banners stream in case you are only
-     * interested in devices located in certain ASNs.
+     * This stream provides a filtered, bandwidth-saving view of the Banners stream in case you are
+     * only interested in devices located in certain ASNs.
      *
      * @param asn    Comma-separated list of ASNs; example "3303,32475"
      * @param apiKey account api key
      */
     @GET("shodan/asn/{asn}")
-    Observable<BannerReport> bannersByAsn(@Path(Constants.ASN) String asn, @Query(Constants.KEY) String apiKey);
+    Observable<BannerReport> bannersByAsn(@Path(Constants.ASN) String asn,
+                                          @Query(Constants.KEY) String apiKey);
 
     /**
-     * This stream provides a filtered, bandwidth-saving view of the Banners stream in case you are only
-     * interested in devices located in certain countries.
+     * This stream provides a filtered, bandwidth-saving view of the Banners stream in case you are
+     * only interested in devices located in certain countries.
      *
-     * @param countries Comma-separated list of countries indicated by their 2 letter code; example "DE,US"
+     * @param countries Comma-separated list of countries indicated by their 2 letter code; example
+     *                  "DE,US"
      * @param apiKey    account api key
      */
     @GET("shodan/countries/{countries}")
@@ -60,13 +88,15 @@ public interface StreamingService {
                                                 @Query(Constants.KEY) String apiKey);
 
     /**
-     * Only returns banner data for the list of specified ports. This stream provides a filtered, bandwidth-saving
-     * view of the Banners stream in case you are only interested in a specific list of ports.
+     * Only returns banner data for the list of specified ports. This stream provides a filtered,
+     * bandwidth-saving view of the Banners stream in case you are only interested in a specific list
+     * of ports.
      *
      * @param ports  Comma-separated list of ports; example "1434,27017,6379"
      * @param apiKey account api key
      */
     @GET("shodan/ports/{ports}")
-    Observable<BannerReport> bannersByPorts(@Path(Constants.PORTS) String ports, @Query(Constants.KEY) String apiKey);
+    Observable<BannerReport> bannersByPorts(@Path(Constants.PORTS) String ports,
+                                            @Query(Constants.KEY) String apiKey);
 
 }
